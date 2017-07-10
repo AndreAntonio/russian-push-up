dfbdeb9a688021e5964bfc55f9f9b20e2085ceca
Julio Brazil <julio_bbl@hotmail.com>
2017-07-10 16:29:33 -0300

    Now we have Vlad

---
+++ russian-push-up/russian-push-up/Vladmir.swift
@@ -0,0 +1,30 @@
+//
+//  Vladmir.swift
+//  russian-push-up
+//
+//  Created by Julio Brazil on 10/07/17.
+//  Copyright © 2017 Andre Faruolo. All rights reserved.
+//
+
+import Foundation
+
+public class Vladmir {
+    var max1: Int = 0
+    var max2: Int = 0
+    var max3: Int = 0
+    
+    let program: [(Double, Int)] = [(0.30, 60),
+                                    (0.50, 60),
+                                    (0.60, 45),
+                                    (0.25, 60),
+                                    (0.45, 30),
+                                    (0.40, 60),
+                                    (0.20, 90),
+                                    (0.35, 45),
+                                    (0.55, 20),
+                                    (0.30, 15),
+                                    (0.65, 60),
+                                    (0.35, 45),
+                                    (0.45, 20),
+                                    (0.25, 120)]
+}
